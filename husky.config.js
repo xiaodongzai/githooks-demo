e379c7d54bf996b6bca915d617d8aab92b063d49
xiezhendong <xiezhendong@360.cn>
2020-09-30 11:42:34 +0800

    ✨ 2234
    www

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lint-staged --verbose',
-    "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
+    "prepare-commit-msg": "exec < /dev/tty && git cz -m 'xx' --hook || true",
   }
 };
